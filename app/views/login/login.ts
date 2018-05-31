4e711ac1d39dfcc709fdeaffbabf4a6d0a3c5d2b
htbkoo <h1017760@connect.hku.hk>
2018-05-31 19:52:13 +0800

    ch-3: 3.4: added data-binding first-native-script-ts

--- app/views/login/login.ts
+++ app/views/login/login.ts
@@ -1,17 +1,25 @@
 const frameModule = require("ui/frame");
+const observableModule = require("data/observable");
+
+const user = new observableModule.fromObject({
+    email: "user@domain.com",
+    password: "password"
+});
+
 let page;
 
-exports.loaded = function(arg) {
+exports.loaded = function (arg) {
     console.log("entered login page");
     page = arg.object;
+    page.bindingContext = user;
 };
 
-exports.signIn = function() {
+exports.signIn = function () {
     let email = page.getViewById("email");
     alert(email.text);
 };
 
-exports.register = function() {
+exports.register = function () {
     const topmost = frameModule.getFrameById('topmost');
     topmost.navigate("views/register/register");
 };
